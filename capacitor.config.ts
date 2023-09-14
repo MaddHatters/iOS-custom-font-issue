2f55bb4d5dc313a1bb166ead02d325bc29d09d17
Matt Beaulieu <30221259+MaddHatters@users.noreply.github.com>
2023-09-14 14:23:07 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ios-select',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
